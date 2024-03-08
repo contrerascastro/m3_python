9fd99dbeb15f2bf9d918825ff651760a9f65140f
contrerascastro <161642444+contrerascastro@users.noreply.github.com>
2024-03-08 10:27:14 -0300

    Add files via upload

---
+++ mi_primer_programa.py
@@ -0,0 +1,13 @@
+nombre = "Marco"
+apellido1 = "Contreras"
+apellido2 = "Castro"
+edad = 43
+estado_civil = "Casado"
+
+print(nombre)
+print(nombre+ " "+apellido1+" "+apellido2)
+
+
+print("Mi edad es",edad,)
+
+print(f"Mi edad es {edad}")
