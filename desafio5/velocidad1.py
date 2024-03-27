ee16486550bf04ece6b38d906e3f59995e52056b
contrerascastro <161642444+contrerascastro@users.noreply.github.com>
2024-03-27 11:32:02 -0300

    Add files via upload

---
+++ desafio5/velocidad1.py
@@ -0,0 +1,18 @@
+#Funciones
+def velocidad_promedio() :
+    promedio = round(sum(velocidad)/len(velocidad))
+    posicion = [velocidad.index(i) for i in velocidad if i > promedio]
+    return posicion
+
+#Datos
+velocidad = [
+    25, 12, 19, 16, 11, 11, 24, 1,
+    14, 14, 16, 10, 6, 23, 13, 25, 4, 19,
+    14, 20, 18, 9, 18, 4, 18, 1, 3, 4, 2,
+    14, 23, 19, 23, 9, 18, 20, 22, 14, 1,
+    10, 5, 23, 3, 5, 9, 5, 3, 12, 20, 5,
+    11, 10, 18, 10, 14, 5, 23, 20, 23, 21
+]
+
+#Resultados
+print(velocidad_promedio())
